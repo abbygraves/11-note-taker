d043eb96c33f7299d047c9af3586cf389ecf1db9
abbygraves <abbygraves14@gmail.com>
2022-07-17 22:18:58 -0500

    removed develop folder

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const db = require('./Develop/db/db.json');
+const db = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));  // ⬅︎ parse incoming string or array data
 app.use(express.json());  // ⬅︎ parse incoming JSON data
-app.use(express.static('./Develop/public'));
+app.use(express.static('./public'));
 
-const { notes } = require('./Develop/db/db.json');
+const { notes } = require('./db/db.json');
 
 const notesArray = [];
 
@@ -17,7 +17,7 @@ const notesArray = [];
 function newNote(data) {
   notesArray.push(data);
   fs.writeFileSync(
-    path.join(__dirname, './Develop/db/db.json'),
+    path.join(__dirname, './db/db.json'),
     JSON.stringify({ notes: notesArray }, null, 2)
   );
 
@@ -43,12 +43,12 @@ app.post('/api/notes', (req, res) => {
 
 // HTML ROUTES
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/Develop/public/notes.html'));
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/Develop/public/index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 
